bc57fe7eba648c81af80f3f891be9304f7f87d41
mzhaonanq <1002604003@qq.com>
2020-08-02 22:17:25 +0800

    mvc1

---
+++ src/app3.js
@@ -0,0 +1,6 @@
+import "./app3.css";
+import $ from "jquery";
+const $square = $(".square");
+$square.on("click", () => {
+  $square.toggleClass("active");
+});
---
+++ src/app4.js
@@ -0,0 +1,8 @@
+import "./app4.css";
+import $ from "jquery";
+const $circle = $(".circle");
+$circle.on("mouseenter", () => {
+  $circle.addClass("active").on("mouseleave", () => {
+    $circle.removeClass("active");
+  });
+});
